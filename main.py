8976bc0fa9812108cbc357b964d704b9ce44a2c2
yttrion <36965563+yttrion@users.noreply.github.com>
2019-05-07 10:54:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import tkinter as Tk
+import sys
+import os
+import threading
+
+class interface:
+    def __init__(self):
+        self.root = Tk.Tk()
+        self.height = 400
+        self.width = 400
+        self.title = self.root.title("Voronoi")
+        self.menubar = Tk.Menu(self.root)
+        
+        self.filemenu = Tk.Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        self.filemenu.add_command(label="Open")
+        self.filemenu.add_command(label="Run")
+        self.filemenu.add_command(label="Quit")
+        
+        self.custommenu = Tk.Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label="Custom", menu=self.filemenu)
+        self.custommenu.add_command(label="Create points")
+        self.custommenu.add_command(label="Drag")
+        
+        self.savemenu = Tk.Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label="Save", menu=self.filemenu)
+        self.savemenu.add_command(label="Save plots")
+        
+
+        #self.can = Tk.Canvas(self.root, height=self.height, width=self.width)
+        #self.can.pack(side=Tk.BOTTOM)
+        self.root.mainloop()
+
+if __name__=="__main__":
+    interface()
