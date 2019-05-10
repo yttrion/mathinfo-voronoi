4e995e75465afe00859ba222551c58f31ba8ea98
yttrion <pierreyvesdouault@gmail.com>
2019-05-10 20:14:15 +0200

    Random Buggy Plotter

--- test/main.py
+++ test/main.py
@@ -5,6 +5,9 @@ import tkinter as tk
 from tkinter import simpledialog
 from tkinter import filedialog
 
+
+from itertools import combinations
+
 import sys
 import os
 import threading
@@ -27,18 +30,22 @@ config.read(configfile)
 def clearScr():
     os.system("clear||cls")
 
-global dots, H, W, offset
+global dots, H, W, offset, flag, tri
+
 dots = []
 H = 400
 W = 400
 offset = 100
-
+tri = None
+flag = True
 
 class Interface:
     def __init__(self):
+        clearScr()
         self.root = tk.Tk()
-        self.height = H
         self.width = W
+        self.height = H
+        self.root.geometry(str(H)+"x"+str(W)+"+100+100")
         self.bg = "white"
         self.title = self.root.title("Voronoi")
         self.menubar = tk.Menu(self.root)
@@ -47,7 +54,7 @@ class Interface:
         self.filemenu = tk.Menu(self.menubar, tearoff=0)
         self.menubar.add_cascade(label="File", menu=self.filemenu)
         self.filemenu.add_command(label="Open", command=self.openfile)
-        self.filemenu.add_command(label="Run")
+        self.filemenu.add_command(label="Run", command=self.drawtri)
         self.filemenu.add_command(label="Quit", command=self.root.destroy)
         
         self.custommenu = tk.Menu(self.menubar, tearoff=0)
@@ -87,7 +94,7 @@ class Interface:
         config.set("config","custom-plot", "0")
         n = simpledialog.askinteger("Number of dots", self.root)
         self.can.delete("all")
-        dots = []
+        dots.clear()
         for k in range(n):
             x, y = random.randint(10,W-10), random.randint(10,H-10)
             self.can.create_oval(x+2,y+2,x-2,y-2, fill="black", tags=str(x)+','+str(y))
@@ -99,6 +106,103 @@ class Interface:
     def keybind(self):
         self.root.bind('<Motion>', self.motion)
         self.root.bind('<Escape>', self.disbalecustom)
+
+
+
+
+
+
+
+
+
+
+
+    def triang(self):
+        global output
+        listetri = list(combinations(dots, 3))
+        flag = True
+        listed=[]
+        trivide = []
+        output = []
+
+        for tri in listetri:
+            #print("Tour: ",tri)
+            #Premiere mediatrice
+            a1 = 2 * (tri[1][0]-tri[0][0])
+            b1 = 2 * (tri[1][1]-tri[0][1])
+            c1 = (tri[0][0])**2 + (tri[0][1])**2 - (tri[1][0])**2 - (tri[1][1])**2
+            #Deuxieme mediatrice
+            a2 = 2 * (tri[2][0]-tri[1][0])
+            b2 = 2 * (tri[2][1]-tri[1][1])
+            c2 = (tri[1][0])**2 + (tri[1][1])**2 - (tri[2][0])**2 - (tri[2][1])**2
+            #Intersection
+            X = (c1*b2 - c2*b1)//(a2*b1 - a1*b2)
+            Y = (c1*a2 - a1*c2)//(a1*b2 - b1*a2)
+            center = [X,Y]
+            r = math.sqrt((tri[1][0] - center[0])**2 + (tri[1][1] - center[1])**2)
+
+            #Verification conditions delaunay
+            for p in dots:
+                if not(p in tri):
+                    dist = (p[0] - center[0])**2 + (p[1] - center[1])**2
+                    listed.append(dist)
+                for d in listed:
+                    if d<=r:
+                        flag=False
+                        break;
+            if flag is True:
+                trivide.append(tri)
+        output = trivide
+    
+    def listcenter(self):
+        global out1
+        liste = []
+        out1 = []
+
+        self.triang()
+
+        for p in output:
+            a1 = 2 * (p[1][0]-p[0][0])
+            b1 = 2 * (p[1][1]-p[0][1])
+            c1 = (p[0][0])**2 + (p[0][1])**2 - (p[1][0])**2 - (p[1][1])**2
+            #Deuxieme mediatrice
+            a2 = 2 * (p[2][0]-p[1][0])
+            b2 = 2 * (p[2][1]-p[1][1])
+            c2 = (p[1][0])**2 + (p[1][1])**2 - (p[2][0])**2 - (p[2][1])**2
+            #Intersection
+            X = (c1*b2 - c2*b1)//(a2*b1 - a1*b2)
+            Y = (c1*a2 - a1*c2)//(a1*b2 - b1*a2)
+            center = [X,Y]
+            liste.append(center)
+        out1 = liste
+    
+        
+    
+    def drawtri(self):
+        self.listcenter()
+        print(out1)
+
+
+        for j in range(len(out1)):
+            x1, y1 = out1[j][0], out1[j][1]
+            #x2, y2 = out1[j-1][0], out1[j-1][1]
+            self.can.create_oval(x1,y1,x1+7,y1+7, fill="red")
+
+
+        #for n in out1:
+        #    self.can.create_line(n[0],n[1], fill="red")
+    
+    
+
+
+
+
+
+
+
+
+
+
 
 
 
